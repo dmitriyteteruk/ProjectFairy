ac9d220309b10f616ac75d6052204ec9d20f036d
Dmitriy Teteruk <dmitriy.teteruk@gmail.com>
2022-11-21 14:24:25 +0300

    ProjectForm descriptoin increase from 1000 to Length(max=10000)

--- fairy/projects/forms.py
+++ fairy/projects/forms.py
@@ -6,8 +6,8 @@ from wtforms.validators import Length, DataRequired
 
 # форма проекта
 class ProjectForm(FlaskForm):
-    name = StringField(label='Название проекта', validators=[DataRequired(), Length(max=100)])
-    description = TextAreaField(label='Описание проекта', validators=[DataRequired(), Length(max=1000)])
+    name = StringField(label='Название проекта', validators=[DataRequired(), Length(max=200)])
+    description = TextAreaField(label='Описание проекта', validators=[DataRequired(), Length(max=10000)])
     delivery_date = DateField(label='Крайний срок сбора подарков на СВХ')
     visit_date = DateField(label='Дата поездки')
     submit = SubmitField(label='Сохранить')
