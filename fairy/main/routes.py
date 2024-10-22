1dd575fa005ab055b9a5a045d4dc994bb1cc600f
DmitryT <34028526+dmitriyteteruk@users.noreply.github.com>
2024-10-22 13:24:48 +0300

    Update routes.py
    Обновлены счетчики для главной страницы, чтобы делить подарки с разных проектов (активных и нет) исходя из текущей даты

--- fairy/main/routes.py
+++ fairy/main/routes.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timedelta
+
 from flask import render_template, Blueprint
 from fairy.models import Gift, Project
 
@@ -8,10 +11,12 @@ main_bp = Blueprint('main_bp', __name__)
 @main_bp.route('/')
 @main_bp.route('/home')
 def home_page():
-    project_name = Project.query.filter(Project.name != None).first()
-    gifts_on_tree = Gift.query.filter(Gift.status == None).all()
-    gifts_total = Gift.query.all()
-    return render_template('home.html', gifts_on_tree=gifts_on_tree, project_name=project_name, gifts_total=gifts_total)
+    active_project = Project.query.filter(Project.delivery_date >= datetime.date.today()).first()
+    gifts_on_tree = Gift.query.filter(Gift.status == None, Gift.project_id == active_project.id).all()
+    gifts_total = Gift.query.filter(Gift.project_id == active_project.id).all()
+    project_date = Project.query.filter(Project.delivery_date).first()
+    today_date = datetime.date.today()
+    return render_template('home.html', gifts_on_tree=gifts_on_tree, active_project=active_project, gifts_total=gifts_total, project_date=project_date, today_date=today_date)
 
 
 # About page route - description about the project
@@ -24,7 +29,3 @@ def about_page():
 @main_bp.route('/contacts')
 def contacts_page():
     return render_template('contacts.html')
-
-
-
-
